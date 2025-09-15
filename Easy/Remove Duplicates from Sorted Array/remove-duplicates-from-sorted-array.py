c5f7adc9df9328ef316be758c6e84c36cd4379e3
Tomás Rodrigues <tomasvrrodrigues@gmail.com>
2025-09-15 02:25:03 +0100

    Remove Duplicates From Sorted Array

---
+++ Easy/Remove Duplicates from Sorted Array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        left, right = 0, 1
+
+        while right <len(nums):
+            if nums[left]==nums[right]:
+                right += 1
+            else:
+                left += 1
+                nums[left]=nums[right]
+        
+        return left+1
